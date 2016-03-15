1c3e09429e572442884cc266f48cb9c02d63bb55
Frank <frank.nijmeijer@efocus.nl>
2016-03-15 14:56:38 +0100

    wrong task name in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var requireDir = require('require-dir');
 requireDir('./gulp-tasks');
 
-gulp.task('default', ['bower', 'scripts', 'sass']);
+gulp.task('default', ['bower', 'scripts', 'styling']);
 
 gulp.task('build-Debug', ['default']);
 gulp.task('build-Release', ['bower', 'scripts-minified', 'sass']);
